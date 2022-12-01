d01602811eb37bc135ec6a6b7a7606e000cd8f10
Akash Raj <akashraj0409@gmail.com>
2022-12-01 22:10:06 +0530

    updated link

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://root:root@cluster0.ug3lcfu.mongodb.net/?retryWrites=true&w=majority", {
+  .connect("mongodb+srv://root:root@cluster0.ug3lcfu.mongodb.net/UserDetailsSignUp?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useCreateIndex:true,
+    // useFindAndModify:false
   })
+  // .connect("mongodb://localhost:27017/UserDetailsSignUp", {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // })
   .then(() => {
     console.log("Connection has been established");
   })
   .catch((e) => {
+    console.log(e);
     console.log("Connection Lost");
   });
